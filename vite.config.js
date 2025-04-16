d57927382b675dc46c3dffea4b9258bfd1ade5f9
namansharma28 <namansharma.web@gmail.com>
2025-04-16 23:21:59 +0530

    deploying react to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -34,5 +34,5 @@ const copyVideoPlugin = () => {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), copyVideoPlugin()],
-  base: "/Nexura-Webdevelopment-final",
+  base: "/Nexura-3D-Department",
 });
